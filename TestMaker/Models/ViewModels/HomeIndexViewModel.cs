7c87f0d723590f38af23a96b54894c2772157962
omin1 <ohmine0704@gmail.com>
2021-05-01 08:29:29 +0900

    create other's question index page and home top page

---
+++ TestMaker/Models/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMaker.Models.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public ICollection<Test> Tests { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
