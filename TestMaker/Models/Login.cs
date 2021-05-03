1257128b92e8c8fa7f4fb5284612c5e743fc6ce7
omin1 <ohmine0704@gmail.com>
2021-05-03 15:50:54 +0900

    Add password confirm input

--- TestMaker/Models/Login.cs
+++ TestMaker/Models/Login.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using TestMaker.Validations;
 
 namespace TestMaker.Models
 {
-    public class NewUserRegist
+    public class Login
     {
         [Required]
         [EmailAddress]
-        [LoginIdUnique]
         public string LoginId { get; set; }
-        public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-        public string Salt { get; set; }
     }
 }
